5d08b2e6a22c7f873df54d19db88efb0eca4e929
Anshul Singh <anshulsinghchambial@gmail.com>
2023-07-14 22:50:25 +0530

    models updated

--- main.py
+++ main.py
@@ -471,11 +471,11 @@ def model_complie(df, model, fs_method, div, analyze, super_label, df_type, doma
      
      # Models
      models = {
-    'lr' :  LogisticRegression(C=500,random_state=0, max_iter=100000),
-    'svc': SVC(probability=True, C= 500, gamma =  0.1, kernel =  'linear'),
-    'knn' :  KNeighborsClassifier(metric = 'minkowski', n_neighbors =  13, weights= 'distance'),
-    'dt' : DecisionTreeClassifier(max_depth=None),
-    'rf' :  RandomForestClassifier(n_estimators=100,
+    "Logistic Regression" :  LogisticRegression(C=500,random_state=0, max_iter=100000),
+    "SVM": SVC(probability=True, C= 500, gamma =  0.1, kernel =  'linear'),
+    "KNN" :  KNeighborsClassifier(metric = 'minkowski', n_neighbors =  13, weights= 'distance'),
+    "Decision Tree" : DecisionTreeClassifier(max_depth=None),
+    "Random Forest" :  RandomForestClassifier(n_estimators=100,
                                   max_features = 'sqrt',
                                   max_depth= None,
                                   bootstrap=True)
